2721d158b7c592eaaa9def3aa991b286cad39e8b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-17 02:15:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
   get '/products/:id/data', to: 'products#data'
-
 end
